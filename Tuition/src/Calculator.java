a0841c73902e0850833e3a1c8bb39fe4c7d56516
zybq <zybq@udel.edu>
2017-02-26 16:36:19 -0500

    quiz2

--- Tuition/src/Calculator.java
+++ Tuition/src/Calculator.java
@@ -10,7 +10,7 @@ public class Calculator {
 		System.out.println("Enter the tuition of your first year in college:");
 		double tuition = input.nextDouble();
 		
-		System.out.println("Enter the percent increase in tuition cost per year(ie: 3 = 3%):");
+		System.out.println("Enter the percent increase in tuition cost per year(ie: 3 = 3%, from 3% to 5%):");
 		double rate = 1 + ((input.nextDouble()) / 100);
 		
 		double total = 0;
@@ -22,7 +22,7 @@ public class Calculator {
 			input.close();
 		}
 		
-		System.out.printf("The total tuition to complete a degree is $%4.2f\n" , total);
+		System.out.printf("The total tuition to complete a degree is $%.2f\n" , total);
 	}
 
 }
